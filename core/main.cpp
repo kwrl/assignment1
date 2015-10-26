30c10a69087b9484625d18b17c40a7f9e46291d8
haakon <haakongk@stud.ntnu.no>
2015-10-26 11:42:56 +0100

    derp|

--- core/main.cpp
+++ core/main.cpp
@@ -21,11 +21,13 @@ void task1a(int argc, char** argv) {
     //First equation.
     convert_to_grayscale(original_img, grayscale);
     show_image(grayscale);
+    cv::imwrite("task1a_avg.jpg", grayscale);
 
     //Second equation.
     cv::Vec3f weights = cv::Vec3f(0.2126f, 0.7152f, 0.0722f);
     convert_to_grayscale_weighted(original_img, grayscale, weights);
     show_image(grayscale); 
+    cv::imwrite("task1a_weighted.jpg", grayscale);
 }
 
 void task2a(int argc, char** argv) {
@@ -36,6 +38,7 @@ void task2a(int argc, char** argv) {
     show_image(grayscale);  //Show the image before inverting it.
     invert(grayscale);
     show_image(grayscale);  //Show the image after inverting it.
+    cv::imwrite("task2a.jpg", grayscale);
 }
 
 void task2b(int argc, char** argv) {
@@ -45,9 +48,24 @@ void task2b(int argc, char** argv) {
 
     show_image(grayscale);  //Show the image before transforming it.
     
-    gamma(grayscale, 1.0f);  
-       
+    gamma(grayscale, 0.2f);  
     show_image(grayscale);  //Show the image after transforming it.
+    cv::imwrite("task2b_02.jpg", grayscale);
+
+    convert_to_grayscale(original_img, grayscale);
+    gamma(grayscale, 0.4f);  
+    show_image(grayscale);  //Show the image after transforming it.
+    cv::imwrite("task2b_04.jpg", grayscale);
+
+    convert_to_grayscale(original_img, grayscale);
+    gamma(grayscale, 0.6f);  
+    show_image(grayscale);  //Show the image after transforming it.
+    cv::imwrite("task2b_06.jpg", grayscale);
+
+    convert_to_grayscale(original_img, grayscale);
+    gamma(grayscale, 0.8f);  
+    show_image(grayscale);  //Show the image after transforming it.
+    cv::imwrite("task2b_08.jpg", grayscale);
 }
 
 void task3a(int argc, char** argv) {
@@ -79,6 +97,8 @@ void task3b(int argc, char** argv) {
     cv::merge(channels, convoluted_img); //Merge channels after applying convolution.
 
     show_image(convoluted_img); //Show the image after transforming it.
+    
+    cv::imwrite("task3b.jpg", convoluted_img);
 }
 
 void task3c(int argc, char** argv) {
@@ -89,61 +109,15 @@ int main(int argc, char** argv) {
         std::cout << "Too few arguments" << std::endl;
         return 1;
     }
-
+    
+    task1a(argc, argv); 
+    task2a(argc, argv); 
+    task2b(argc, argv); 
+    task1a(argc, argv); 
+    task1a(argc, argv); 
+    task3a(argc, argv);
     task3b(argc, argv);
-    return 0;
+    task3c(argc, argv);
 
-    cv::Mat originalImg, convolutedImg, kernel, grayscale, gammaImg;
-    load_image(originalImg, argv[1]);
-    show_image(originalImg);
-    
-    convert_to_grayscale(originalImg, grayscale);
-    show_image(grayscale);
-
-    gamma(grayscale, 0.8);
-    show_image(grayscale);
-    
-    
-    /*
-    uchar data[3][3] = {
-        {1, 2, 1},
-        {2, 4, 2},
-        {1, 2, 1},
-    };
-    kernel = cv::Mat(3, 3, CV_8UC1, &data);
- 
-    load_image(originalImg, argv[1]);
-    show_image(originalImg);
-
-    for(int ii = 0; ii < 3; ii++) {
-        for(int jj= 0; jj < 3; jj++) {
-            auto color = originalImg.at<cv::Vec3b>(ii,jj);
-            std::cout << static_cast<unsigned int>(color[2]) << "\t";
-        }
-        std::cout << std::endl;
-    }
-
-    std::vector<cv::Mat> channels(3);
-    cv::split(originalImg, channels);
-
-    convolution(channels[0], convolutedImg, kernel, 1.0f/16.0f);
-    channels[0] = convolutedImg;
-    convolution(channels[1], convolutedImg, kernel, 1.0f/16.0f);
-    channels[1] = convolutedImg;
-    convolution(channels[2], convolutedImg, kernel, 1.0f/16.0f);
-    channels[2] = convolutedImg;
-
-    cv::merge(channels, originalImg);
-
-    for(int ii = 0; ii < 3; ii++) {
-        for(int jj= 0; jj < 3; jj++) {
-            auto color = originalImg.at<cv::Vec3b>(ii,jj);
-            std::cout << static_cast<unsigned int>(color[2]) << "\t";
-        }
-        std::cout << std::endl;
-    }
-
-    show_image(originalImg); 
-    */
     return 0;
 }
